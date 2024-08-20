3841319a97068e9e0e4cc034bc82f841addf1d8e
Braian A. Diez <bdiez19@gmail.com>
2024-08-20 03:07:18 -0300

    feat(ui):: Add header and hero components to index page

---
+++ components/Header.vue
@@ -0,0 +1,10 @@
+<template>
+<header class="w-full h-32 flex items-center justify-between border-b border-green-500 px-5 py-6 bg-black">
+  <uiLogo/>
+  <nav class="font-mono flex items-center justify-center gap-x-16 text-2xl">
+  <p class="">Candidatos</p>
+  <p class="">Categorías</p>
+  <p class="">Login</p>
+  </nav>
+</header>
+</template>
---
+++ components/ui/Logo.vue
@@ -0,0 +1,13 @@
+<template>
+  <div
+    class="bg-green-500 text-black flex items-start justify-center flex-col
+    text-4xl font-black font-display leading-none h-full px-3 relative">
+    <p class="">DISCORD</p>
+    <p class="">AWARDS</p>
+    <div class="absolute -top-3 -right-3 rounded-full overflow-hidden aspect-square size-11 border border-white">
+      <NuxtImg
+        src="/assets/midustampa.jpg"
+        aria-label="MiduDev estampita"/>
+    </div>
+  </div>
+</template>
